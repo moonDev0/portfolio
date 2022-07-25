ddffe313aa0e6bf645719fefbb96652631fd52a3
moondev0 <elmasinc02@gmail.com>
2022-07-25 07:54:00 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const navbar = () => {
     <div className="n-wrapper" id="Navbar">
       {/* left */}
       <div className="n-left">
-        <div className="n-name">Andrew</div>
+        <div className="n-name">Munir</div>
         <Toggle />
       </div>
       {/* right */}
